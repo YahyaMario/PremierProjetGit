486ad27caba8b35e54b38cce2b8acbc584a44e41
Abdellatif Saidi <abdellatif.saidi@calq.gouv.qc.ca>
2022-12-21 15:28:53 -0500

    modification

--- src/main/java/git/Test.java
+++ src/main/java/git/Test.java
@@ -2,7 +2,7 @@ package main.java.git;
 
 public class Test {
     public static void main(String args[]){
-
+        System.out.println("nouveau commit");
         System.out.println("Hello word");
     }
 }
